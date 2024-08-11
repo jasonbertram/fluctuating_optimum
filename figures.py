b496d6d9e4f1fc6a79167e5618cf4397045b8d17
Jason Bertram <jason.bertram@uwo.ca>
2024-08-10 22:18:44 -0400

    L

--- figures.py
+++ figures.py
@@ -4,14 +4,14 @@ Created on Tue May 30 15:27:44 2023
 
 @author: jason
 """
-#Theory functions
-#======================
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import integrate
 import scipy.optimize
 import ast
-                  
+
+#Theory functions
+#======================                  
 
 def phi_not_normed(x,N,mu,sigma_c2,s):
     
@@ -160,3 +160,250 @@ xs=10**(np.linspace(-4,-1))
 h2_array=np.array([[heritability(10,y,x) for x in xs] for y in ys])
 pos=plt.contourf(np.linspace(-4,-1),np.log10(ys),h2_array,levels=np.arange(0,np.max(h2_array)+0.05,0.05))
 fig.colorbar(pos)
+
+#%%
+#Stablizing picture
+
+def Vg(mu,L,Vs):
+    return 4*mu*L*Vs
+
+x=np.linspace(-2,2.1,1000)
+
+fig,ax=plt.subplots(figsize=[4,2.7])
+
+ax.plot(x,np.exp(-x**2),'r',label='Fitness')
+ax.plot(x,np.exp(-10*x**2),'b',label='Population')
+
+ax.set_xticklabels([])
+ax.set_xlabel(r'Trait value',fontsize=14)
+
+ax.set_yticklabels([])
+ax.set_ylabel(r'Distribution',fontsize=14)
+
+xpos=0.4
+plt.annotate(s='', xy=(-xpos,np.exp(-10*xpos**2)), xytext=(xpos,np.exp(-10*xpos**2)), arrowprops=dict(arrowstyle='<->'))
+plt.annotate(s=r'$V_g+V_e$',xy=(-0.435,np.exp(-0.15*xpos**2)-0.15),xytext=(-0.435,np.exp(-10*xpos**2)-0.15),fontsize=14)
+
+
+xpos=0.6
+plt.annotate(s='', xy=(-xpos,np.exp(-xpos**2)), xytext=(xpos,np.exp(-xpos**2)), arrowprops=dict(arrowstyle='<->'))
+plt.annotate(s=r'$V_s$',xy=(-0.15,np.exp(-xpos**2)-0.15),xytext=(-0.15,np.exp(-xpos**2)-0.15),fontsize=14)
+
+
+plt.annotate(s=r'Trait optimum',xy=(0,1),xytext=(0,1.1),fontsize=12,arrowprops=dict(arrowstyle='->'))
+
+plt.tight_layout()
+
+plt.legend(loc='upper left',frameon=False)
+
+plt.savefig("stablizing.eps")
+
+#%%
+#Latter-Bulmer predictions
+
+mu=6.6e-9
+
+Ls=np.linspace(1.2e4,1.2e8,10000)
+plt.plot(np.log10(Ls),Vg(mu,Ls,20)/(1+Vg(mu,Ls,20)),label=r'$V_s=20V_e$')
+plt.plot(np.log10(Ls),Vg(mu,Ls,5)/(1+Vg(mu,Ls,5)),label=r'$V_s=5V_e$')
+
+plt.fill_between(np.log10(Ls),[0.1],[0.6],alpha=0.5)
+
+plt.xlabel(r'Target size (fraction of euchromatic genome)',fontsize=14)
+plt.ylabel(r'Heritability',fontsize=14)
+
+plt.gca().set_xticklabels(['',r'$10^{-4}$','',r'$10^{-3}$','',r'$10^{-2}$','',r'$10^{-1}$','',r'$1$'],fontsize=14)
+plt.yticks(fontsize=14)
+
+plt.legend(fontsize=14)
+
+
+plt.ylim([0,1])
+plt.tight_layout()
+plt.savefig("LB.pdf")
+
+#%%
+#Finite N Latter-Bulmer predictions
+mu=6.6e-9
+
+Ls=np.linspace(1.2e4,1.2e8,10000)
+plt.plot(np.log10(Ls),Vg(mu,Ls,20)/(1+Vg(mu,Ls,20)),label=r'$V_s=20V_e, N\rightarrow\infty$')
+plt.plot(np.log10(Ls),Vg(mu,Ls,5)/(1+Vg(mu,Ls,5)),label=r'$V_s=5V_e, N\rightarrow\infty$')
+
+plt.fill_between(np.log10(Ls),[0.1],[0.6],alpha=0.5)
+
+plt.xlabel(r'Target size (fraction of euchromatic genome)',fontsize=14)
+plt.ylabel(r'Heritability',fontsize=14)
+
+plt.gca().set_xticklabels(['',r'$10^{-4}$','',r'$10^{-3}$','',r'$10^{-2}$','',r'$10^{-1}$','',r'$1$'],fontsize=14)
+plt.yticks(fontsize=14)
+
+plt.gca().set_prop_cycle(None)
+
+Ls=np.array([10,50,1e2,5e2,1e3,5e3,1e4,5e4,1e5])
+mu=mu*1e3
+
+h2_theory=[]
+for _ in Ls:
+
+    temp=Vg_pred_consistent(1e-1,1000,mu,0.3,_,0,20)
+    h2_theory.append(temp/(1+temp))
+
+plt.plot(np.log10(Ls)+3,h2_theory,'--',label=r'$V_s=20V_e, N=1000$')
+
+
+h2_theory=[]
+for _ in Ls:
+
+    temp=Vg_pred_consistent(1e-1,1000,mu,0.3,_,0,5)
+    h2_theory.append(temp/(1+temp))
+
+plt.plot(np.log10(Ls)+3,h2_theory,'--',label=r'$V_s=5V_e, N=1000$')
+plt.ylim([0,1])
+plt.tight_layout()
+plt.legend(fontsize=14)
+
+#plt.savefig("LB_drift.pdf")
+
+#%%
+#Moving optimum picture
+
+x=np.linspace(-2,2.1,1000)
+
+fig,ax=plt.subplots(figsize=[4,2.7])
+
+ax.plot(x,np.exp(-(x-0.6)**2),'r',label='Fitness')
+ax.plot(x,np.exp(-10*x**2),'b',label='Population')
+
+ax.set_xticklabels([])
+ax.set_xlabel(r'Trait value',fontsize=14)
+
+#ax.set_yticklabels([])
+ax.set_ylabel(r'Distribution',fontsize=14)
+
+xpos=0.4
+plt.annotate(s=r'', xy=(-xpos+0.6,1.05), xytext=(xpos+0.6,1.05), arrowprops=dict(arrowstyle='<->',linewidth='2'))
+
+plt.annotate(s='Trait\noptimum',xy=(0.6,1),xytext=(0.22,.6),fontsize=12,arrowprops=dict(arrowstyle='->'))
+
+plt.ylim([0,1.1])
+
+plt.tight_layout()
+
+plt.legend(loc='upper left',frameon=False)
+
+plt.savefig("stablizing_fluc.eps")
+
+#%%
+#Chasing optimim picture
+
+x=np.linspace(-2,2.1,1000)
+
+fig,ax=plt.subplots(figsize=[4,2.7])
+
+ax.plot(x,np.exp(-(x-1.5)**2),'r',label='Fitness')
+ax.plot(x,np.exp(-10*x**2),'b',label='Population')
+
+ax.set_xticklabels([])
+ax.set_xlabel(r'Trait value',fontsize=14)
+
+#ax.set_yticklabels([])
+ax.set_ylabel(r'Distribution',fontsize=14)
+
+xpos=0.4
+plt.annotate(s='', xy=(-0.5,1.0), xytext=(0.8,1.0), arrowprops=dict(arrowstyle='<-',linewidth='2'))
+
+plt.annotate(s=r'Rate $\propto V_g$',xy=(0.,1.025), xytext=(-0.4,1.025))
+
+plt.ylim([0,1.1])
+
+plt.tight_layout()
+
+plt.legend(loc='upper left',frameon=False)
+
+plt.savefig("stablizing_lande.eps")
+
+
+#%%
+#Latter-Bulmer_fluctuation predictions 
+
+sigma2=1e-2
+theta=0e-2
+
+plt.figure(figsize=[3,2])
+ax=plt.gca()
+
+opts=np.zeros(1000)
+for _ in range(1,1000):
+    opts[_]=(1-theta)*opts[_-1] + np.random.normal(0,np.sqrt(sigma2))
+
+ax.set_yticklabels([])
+ax.set_xticklabels([])
+
+ax.set_ylabel('Trait optimum')
+ax.set_xlabel('Time')
+
+plt.plot(opts)
+plt.tight_layout()
+
+plt.savefig("random_walk.pdf")
+
+#%%
+mu=6.6e-9
+
+Ls=np.linspace(1.2e4,1.2e8,10000)
+plt.plot(np.log10(Ls),Vg(mu,Ls,20)/(1+Vg(mu,Ls,20)),label=r'$V_s=20V_e, \sigma^2=0$')
+plt.plot(np.log10(Ls),Vg(mu,Ls,5)/(1+Vg(mu,Ls,5)),label=r'$V_s=5V_e, \sigma^2=0$ ')
+
+
+Ls=np.array([10,25,50,75,1e2,2.5e2,5e2,7.5e2,1e3,2.5e3,5e3,7.5e3,1e4,5e4,1e5])
+mu=mu*1e3
+
+# h2_theory=[]
+# for _ in Ls:
+#     temp=Vg_pred_consistent(1e-1,1000,mu,0.3,_,0,20)
+#     h2_theory.append(temp/(1+temp))
+
+# plt.plot(np.log10(Ls)+3,h2_theory,label=r'$V_s=20V_e, N=1000$')
+
+
+# h2_theory=[]
+# for _ in Ls:
+#     temp=Vg_pred_consistent(1e-1,1000,mu,0.3,_,0,5)
+#     h2_theory.append(temp/(1+temp))
+
+# plt.plot(np.log10(Ls)+3,h2_theory,label=r'$V_s=5V_e, N=1000$')
+
+plt.gca().set_prop_cycle(None)
+
+h2_theory=[]
+for _ in Ls:
+
+    temp=Vg_pred_consistent(1e-1,10000,mu,0.3,_,1e-3,20)
+    h2_theory.append(temp/(1+temp))
+
+plt.plot(np.log10(Ls)+3,h2_theory,'--',label=r'$V_s=20V_e, \sigma^2=10^{-2}V_e$')
+
+
+h2_theory=[]
+for _ in Ls:
+
+    temp=Vg_pred_consistent(1e-1,10000,mu,0.3,_,1e-3,5)
+    h2_theory.append(temp/(1+temp))
+
+plt.plot(np.log10(Ls)+3,h2_theory,'--',label=r'$V_s=5V_e, \sigma^2=10^{-2}V_e$')
+
+
+plt.fill_between(np.log10(Ls)+3,[0.1],[0.6],alpha=0.5)
+
+plt.xlabel(r'Target size (fraction of euchromatic genome)',fontsize=14)
+plt.ylabel(r'Heritability',fontsize=14)
+
+plt.gca().set_xticklabels(['',r'$10^{-4}$','',r'$10^{-3}$','',r'$10^{-2}$','',r'$10^{-1}$','',r'$1$'],fontsize=14)
+plt.yticks(fontsize=14)
+
+plt.ylim([0,1])
+plt.tight_layout()
+plt.legend(fontsize=12)
+
+#plt.savefig("LB_fluc.pdf")
