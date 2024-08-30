90741b93fa166ed030a63cd6473a61fb1e7c3f62
Jason <jason.bertram@uwo.ca>
2024-08-30 15:11:23 -0400

    full paramters + cleanup

--- simulate_multiproc.py
+++ simulate_multiproc.py
@@ -4,9 +4,6 @@ Created on Thu May 30 14:31:48 2024
 
 @author: jason
 """
-
-
-
 import numpy as np
 import itertools
 from multiprocessing import Pool
@@ -25,7 +22,6 @@ def p_prime_sel_opt(p,delt_opt,gam,sign,V_s):
     S=1/(2*V_s)
     p=pmap(rhomap(p)*np.exp(2*S*gam*sign*(delt_opt+0.5*gam*sign*(2*p-1))))    
     return p
-
 
 def simulate(param):
     
@@ -46,7 +42,6 @@ def simulate(param):
     
     for t in range(maxiter):
         
-        
         fixed_loci_1=(p==1)
         
         #Reset fixed loci and remove them from optimum
@@ -54,7 +49,6 @@ def simulate(param):
         opt=opt-2*a*np.sum(sign*fixed_loci_1,0)
         
         zbar=np.sum(2*a*sign*p**2+a*sign*p*(1-p),0)
-        
         
         fixed_loci_0=(p==0)
         mutation_mask=((np.random.rand(L,rep)<N*mu) & fixed_loci_0)
@@ -69,7 +63,6 @@ def simulate(param):
             (np.random.rand(np.sum(poly_loci))<N*mu*(1-p[poly_loci]))/N-\
                 (np.random.rand(np.sum(poly_loci))<N*mu*p[poly_loci])/N
         
-        
         p=np.random.binomial(N,p_prime_sel_opt(p,opt-zbar,a,sign,V_s))/N
         #p=np.random.binomial(N,p,size=sim_L)/N
         
@@ -79,39 +72,26 @@ def simulate(param):
     
     return 2*a**2*np.sum(p*(1-p),0)
 
-
-
 ####################################
 #Parallel simulator. One core per parameter vector.
-#Will not run in interactive mode
+#Will not run in interactive mode (ipython)
+#Have not tested on more than one node
 
-
-
-sigma_e2s=np.array([0,1e-6,1e-5,1e-4,1e-3,1e-2])
-
-Ls=np.array([100])
-Ns=np.array([10000])
-Vs=np.array([5])
+sigma_e2s=np.array([0,1e-4,5e-4,1e-3,5e-3,1e-2])
+Ls=np.array([100,1000])
+Ns=np.array([1000,10000])
+Vs=np.array([5,20])
 mus=np.array([5e-6])
 thetas=np.array([0e-2])
 a2s=np.array([0.01,0.02,0.04,0.06,0.08,0.1])
 #Mutational heritability = 2 L mu alpha**2
 #Vms=np.array([1e-4])
-
 reps=np.array([1000])
 
 params=[_ for _ in itertools.product(Ls,sigma_e2s,Ns,Vs,mus,a2s,thetas,reps)]
-
 
 if __name__ == '__main__':
     p=Pool(32)
     output=np.array(p.map(simulate, params))
     
-
 np.savetxt("Vg_sims",output,header=str(params))
-
-#plot_params=[_ for _ in itertools.product(Ls,Ns,Vs,mus,Vms,reps)]
-#np.savetxt("Vg_sims",output,header=str(list(sigma_e2s))+'\n'+str(plot_params))
-
-
-    
