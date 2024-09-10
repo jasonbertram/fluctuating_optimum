d9d9145541756e927a88c8f1df946d89341253d5
Jason Bertram <jbertr26@gra-login3.graham.sharcnet>
2024-09-10 19:47:47 -0400

    mpi functioning

--- mpi_job.sh
+++ mpi_job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-#SBATCH --ntasks=4               # number of MPI processes
-#SBATCH --mem-per-cpu=1G      # memory; default unit is megabytes
-#SBATCH --time=0-00:05           # time (DD-HH:MM)
+#SBATCH --ntasks=100               # number of MPI processes
+#SBATCH --mem-per-cpu=2G      # memory; default unit is megabytes
+#SBATCH --time=0-06:00           # time (DD-HH:MM)
 
 module load scipy-stack mpi4py
 
-srun python ./test.py
+srun python simulate_mpi.py
