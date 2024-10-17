27a26ab5a83df8c347967b6da04cfe7e5d162947
Jason <jason.bertram@uwo.ca>
2024-10-17 11:58:17 -0400

    l

--- final_approximation.py
+++ final_approximation.py
@@ -1,40 +1,51 @@
 import numpy as np
 import matplotlib.pyplot as plt
 N=10000
-sigma2=1e-1
+sigma2=1e-2
 a2=1e-1
 Vs=5
 ss=a2/(2*Vs)
+L=100
 
 def sigma_a2(a2, sigma2, Vg):
     return a2*sigma2/Vg**2
 
 def bbar(ss, a2, sigma2, Vg):
-    b=ss/sigma_a2(a2, sigma2, Vg)
+    b=2*ss/sigma_a2(a2, sigma2, Vg)
     return b/(1+d(a2, sigma2, Vg))
 
 def d(a2,sigma2,Vg):
     return -.5 + np.sqrt(1/4 + 1/(N*sigma_a2(a2,sigma2,Vg)))
 
-C=4*N*100*a2*5e-6
+C=4*N*L*a2*5e-6
 
 plt.figure()
-x=np.linspace(1e-2,.2,1000)
-plt.plot(x,x**-0*C*d(a2,sigma2,x)**bbar(ss,a2,sigma2,x)\
-        /(d(a2,sigma2,x)-1) \
+x=np.linspace(1e-2,1,1000)
+plt.plot(x,C*d(a2,sigma2,x)**bbar(ss,a2,sigma2,x)\
+        /(bbar(ss,a2, sigma2, x) -1) \
         *(d(a2,sigma2,x)**(1-bbar(ss,a2,sigma2,x)) \
         -(0.5+d(a2,sigma2,x))**(1-bbar(ss,a2,sigma2,x))) \
         )
-plt.plot(x,x**-0*C*d(a2,sigma2,x)**bbar(ss,a2,sigma2,x)\
-        /(0*d(a2,sigma2,x)-1)*-0.5)
+    
+plt.plot(x,C*d(a2,sigma2,x)\
+        /(bbar(ss,a2, sigma2, x) -1) \
+        *(1\
+        -0*(0.5/d(a2,sigma2,x)+1)**(1-bbar(ss,a2,sigma2,x)))\
+        )
 
-plt.plot(x,0.5*C*d(a2,sigma2,0.1)**bbar(ss,a2,sigma2,x),'k')
+plt.plot(x,C*d(a2,sigma2,x)\
+        /(bbar(ss,a2, sigma2, x) -1)-x \
+        *(1\
+        -0*(0.5/d(a2,sigma2,x)+1)**(1-bbar(ss,a2,sigma2,x)))\
+        )
+
+
 
 #plt.plot(x,0.5*C*(1-0.5*np.log(N*0.1**2/(sigma2*a2))*(ss/(a2*sigma2))*x**2))
 plt.plot(x,x)
 #plt.ylim([-0.0,0.2])
 
-plt.savefig('/home/jason/git/fluctuating_optimum/temp.pdf')
+#plt.savefig('/home/jason/git/fluctuating_optimum/temp.pdf')
 
 #plt.plot(x,np.log(x**-0*4*N*100*a2*5e-6*d(a2,sigma2,x)**bbar(ss,a2,sigma2,x)\
 #        /(d(a2,sigma2,x)-1) \
