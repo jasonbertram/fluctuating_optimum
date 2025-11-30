f2a47ee9231d4c8bf44da03532feb2aaa8264f20
Jason Bertram <jbertr26@l4.nibi.sharcnet>
2025-11-29 21:40:37 -0500

    sims for new figure

--- simulate_mpi.py
+++ simulate_mpi.py
@@ -75,10 +75,13 @@ def simulate(param):
 #Parallel handling of replicates with MPI.
 
 #sigma_e2s=np.array([0,1e-4,5e-4,1e-3,5e-3,1e-2])
-sigma_e2s=np.array([1e-2])
-Ls=np.array([100])
-Ns=np.array([100,200,500,1000,2000,5000,10000,50000,100000])
+sigma_e2s=np.array([1e-3])
+#Ls=np.array([100])
+Ls=np.array([10,20,50,100,200,500,1000])
+Ns=np.array([10000])
+#Ns=np.array([100,200,500,1000,2000,5000,10000,50000,100000])
 Vs=np.array([5])
+#Vs=np.linspace(1,20,9)
 mus=np.array([6.6e-6])
 #mus=np.array([1e-7,2e-7,5e-7,1e-6,2e-6,5e-6,1e-5,2e-5,5e-5])
 thetas=np.array([0e-1])
